3fe632e115be029055ff2ce8d5c59d9c713c3757
CuityMiko <15201776956@163.com>
2018-04-05 10:39:13 +0800

    Update 2018-04-05T10:39:13.500788

--- count.py
+++ count.py
@@ -8,7 +8,7 @@ def change_time():
   return datetime.timedelta(minutes=1) 
 
 now = datetime.datetime.now() 
-commit_date = (now.replace(day=now.day-15) + change_day()) 
+commit_date = (now.replace(day=now.day-12) + change_day()) 
 print change_day()
 print commit_date
 print now
