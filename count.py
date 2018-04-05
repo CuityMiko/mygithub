80891c0a2252c8867b5b35ba98b2a88f3eb38ce0
CuityMiko <15201776956@163.com>
2018-04-05 10:44:16 +0800

    Update 2018-04-05T10:44:16.685005

--- count.py
+++ count.py
@@ -22,4 +22,5 @@ while commit_date < now:
     f.close()
     os.system('git add .') 
     os.system('git commit --date={time} -m "Update {time}"'.format(time=commit_date.isoformat()))
+os.system('git pull')
 os.system('git push')
